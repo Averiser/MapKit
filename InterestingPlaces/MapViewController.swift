d99580edf4078279854c6b294d938d867f2b7f12
Averiser <averiser@gmail.com>
2022-01-09 15:31:19 +0300

    second commit - Centering the Map

--- InterestingPlaces/MapViewController.swift
+++ InterestingPlaces/MapViewController.swift
@@ -7,24 +7,38 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+  @IBOutlet weak var mapView: MKMapView!
+  
+  @IBAction func changeMapType(_ sender: UISegmentedControl) {
+    
+    if sender.selectedSegmentIndex == 0 {
+      mapView.mapType = .standard
+    } else {
+      mapView.mapType = .satellite
     }
     
+  }
+  @IBAction func close(_ sender: Any) {
+    dismiss(animated: true, completion: nil)
+  }
+  override func viewDidLoad() {
+        super.viewDidLoad()
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    let appleHQ = CLLocation(latitude: 37.334722, longitude: -122.008889)
+    let regionRadius: CLLocationDistance = 1000.0
+    let region = MKCoordinateRegion(center: appleHQ.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+    mapView.setRegion(region, animated: true)
+    mapView.delegate = self
     }
-    */
 
+}
+
+extension MapViewController: MKMapViewDelegate {
+  func mapViewWillStartRenderingMap(_ mapView: MKMapView) {
+    print("rendering ...")
+  }
 }
